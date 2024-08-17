bbedf6753d46bdf52e576ee840c7b95947921b4b
eliseyangg <93101453+eliseyangg@users.noreply.github.com>
2024-08-17 02:19:14 -0400

    initial revamp update assets

---
+++ assets/js/stl/truck.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    // truck
+    STLViewer("../assets/stl/GARBAGETRUCK.STL", "assytruck","#40826D");
+    STLViewer("../assets/stl/BASE.STL", "base","#FFFDD0");
+    STLViewer("../assets/stl/CONTAINER.STL", "container","#6FC276");
+    STLViewer("../assets/stl/DOOR.STL", "door","#6FC276");
+    STLViewer("../assets/stl/FRONTWHEELS.STL", "front","#3A3B3C");
+    STLViewer("../assets/stl/REARWHEELS.STL", "rear","#3A3B3C");
+    STLViewer("../assets/stl/YELLOWGRABBER1.STL", "yellow1","#F8DE7E");
+    STLViewer("../assets/stl/YELLOWGRABBER2.STL", "yellow2","#F8DE7E");
+    STLViewer("../assets/stl/YELLOWGRABBER3.STL", "yellow3","#F8DE7E");
+};
